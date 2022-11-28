0eea76bb8e7ec7458f3a1025e20a290baf62c8df
BenRGagnon <benjamin.r.gagnon@gmail.com>
2022-11-28 13:15:41 -0500

    Corrected syntax error for pushing to edit meeting

---
+++ virus_validate/lib/pages/meeting_details.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:virus_validate/models/meeting_model.dart';
+import 'package:virus_validate/pages/edit_meeting_details.dart';
+
+class EmployeeMeetingDetails extends StatefulWidget {
+  const EmployeeMeetingDetails({super.key, required this.meeting});
+  final Meeting meeting;
+  
+  @override
+  State<StatefulWidget> createState() => _EmployeeMeetingDetailsState();
+
+}
+
+class _EmployeeMeetingDetailsState extends State<EmployeeMeetingDetails> {
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => EditMeetingPage(meeting: widget.meeting))
+              );
+            },
+            child: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+    );
+  }
+}
