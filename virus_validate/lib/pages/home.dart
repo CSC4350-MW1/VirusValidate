49b29b9ab0a3be07312b23439fd7a3b5de7a2297
BenRGagnon <benjamin.r.gagnon@gmail.com>
2022-11-28 12:45:05 -0500

    Updated guest homepage to mostly match the employee homepage with different links

--- virus_validate/lib/pages/home.dart
+++ virus_validate/lib/pages/home.dart
@@ -3,9 +3,6 @@ import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:virus_validate/firestoreExample/firestore_example.dart';
-import 'package:virus_validate/firestore_service.dart';
-import 'package:virus_validate/forms/new_meeting_form.dart';
 import 'package:virus_validate/helpers/meeting_card.dart';
 import 'package:virus_validate/models/meeting_model.dart';
 import 'package:virus_validate/pages/edit_meeting_details.dart';
@@ -22,7 +19,6 @@ class EmployeeHomePage extends StatefulWidget {
 
 class _EmployeeHomeState extends State<EmployeeHomePage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirestoreService _fs = FirestoreService();
 
   late Stream<QuerySnapshot> _meetingStream;
 
@@ -131,35 +127,101 @@ class GuestHomePage extends StatefulWidget {
 }
 
 class _GuestHomePageState extends State<GuestHomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late Stream<QuerySnapshot> _meetingStream;
+
+  @override
+  void initState() {
+    super.initState();
+    String uid = _auth.currentUser!.uid;
+    log(uid);
+    _meetingStream = FirebaseFirestore.instance
+      .collection('Meetings')
+      .where("employee", isEqualTo: uid)
+      .snapshots();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Guest Homepage'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  
-                },
-                child: const Text('Health Screening'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  
-                },
-                child: const Text('Unlock Door')
-              ),
-            ),
-          ],
+        title: const Text("Meetings"),
+        // Logout Button
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child: GestureDetector(
+            onTap: () {
+              _auth.signOut();
+            },
+            child: const Icon(Icons.logout_sharp),
+          ),
         ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => const GuestHomePage())
+              );
+            },
+            child: const Icon(Icons.person),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => const SymptomQuestionnaire())
+              );
+            },
+            child: const Icon(Icons.coronavirus_rounded),
+          ),
+          // Create New Meeting
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => const NewMeetingPage()));
+              },
+              child: const Icon(Icons.note_add_outlined),
+            ),
+          ),
+        ],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _meetingStream,
+        builder: ((BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Error Occurred");
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting || !snapshot.hasData) {
+            return const Loading();
+          }
+
+          return ListView(
+          children: snapshot.data!.docs
+              .map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                Meeting meeting = Meeting.fromJson(document.id, data);
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                    context, 
+                    MaterialPageRoute(builder: (context) => EditMeetingPage(meeting: meeting))
+                    );
+                  },
+                  child: MeetingCard(meeting: meeting)
+                );
+              })
+              .toList()
+              .cast(),
+        );
+
+        }),
       ),
     );
   }
