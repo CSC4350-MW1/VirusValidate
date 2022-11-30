c09ee725fcb17c60223de5adf821dcdf89019cdb
BenRGagnon <benjamin.r.gagnon@gmail.com>
2022-11-29 22:53:08 -0500

    Added name to employee model

--- virus_validate/lib/models/employee_model.dart
+++ virus_validate/lib/models/employee_model.dart
@@ -1,19 +1,22 @@
 class Employee {
   final String id;
+  final String name;
   final String email;
   
   Employee({
     required this.id,
-    required this.email
+    required this.email,
+    required this.name
   });
 
   factory Employee.fromJson(String id, Map<String, dynamic> data) {
-    return Employee(id: id, email: data["email"]);
+    return Employee(id: id, email: data["email"], name: data["name"]);
   }
 
   Map<String, dynamic> toJson() {
     return {
-      "email": email
+      "email": email,
+      "name": name
     };
   }
 }
